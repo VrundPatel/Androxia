7aaa9b5845102a4bb2de45f99140f64d10e3b681
Vrund Patel <vrundp07@yahoo.com>
2017-12-08 09:57:19 -0500

    Added subscription and contact form modals.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener
+} from '@angular/core';
+
+import { WOW } from 'wowjs/dist/wow.min';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit() {
+    console.log(window.pageYOffset);
+    const wow = new WOW();
+    wow.init();
+  }
+
+  constructor(public el: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+    console.log('hello', scrollPosition);
+
+    // if (scrollPosition >= componentPosition) {
+    //   this.state = 'show'
+    // } else {
+    //   this.state = 'hide'
+    // }
+
+  }
+
 }
-
-

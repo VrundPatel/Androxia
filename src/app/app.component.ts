27062f1fe392b3396604fe8e46e5e69cf1169324
Vrund Patel <vrundp07@yahoo.com>
2017-12-19 23:14:12 -0500

    WIP for initial release.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,23 @@
-import {
-  Component,
-  ElementRef,
-  HostListener
-} from '@angular/core';
-
-import { WOW } from 'wowjs/dist/wow.min';
-import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'app';
+export class AppComponent  {
 
-  ngAfterViewInit() {
-    console.log(window.pageYOffset);
-    const wow = new WOW();
-    wow.init();
-  }
-
-  constructor(public el: ElementRef) {}
-
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    const componentPosition = this.el.nativeElement.offsetTop;
-    const scrollPosition = window.pageYOffset;
-    console.log('hello', scrollPosition);
-
-    // if (scrollPosition >= componentPosition) {
-    //   this.state = 'show'
-    // } else {
-    //   this.state = 'hide'
-    // }
-
-  }
-
+  team = [
+    {
+      'name': 'Asutosh Patel',
+      'title': 'Co-Founder',
+      'imageUrl': '',
+      'aboutMe': '',
+      'facebookLink': '',
+      'twitterLink': '',
+      'linkedInLink': '',
+      'youtubeLink': '',
+      'githubLink': ''
+    }
+  ]
 }
